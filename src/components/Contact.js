d9ad2f104d9c2722d4d1c527b6f7ea0479c1707f
gtn777 <gtn777@orange.fr>
2020-04-06 20:26:02 +0200

    quest 5 done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "./ContactStyle.css";
 
-const Contact = props => {
-  return (
+
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      connected: this.props.online,  
+    };
+  }
+  render() {return (
     <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name} />
+      <img className="avatar" src={this.props.avatar} alt={this.props.name} />
       <div>
-        <p className="name">{props.name}</p>
-        <div className="status">
-          <p className={props.online ? "status-online" : "status-offline"}> </p>
-          <p className="status-text"> {props.online ? "OnLine" : "OffLine"} </p>
+        <p className="name">{this.props.name}</p>
+        <div onClick={() => {var newStatus = !this.state.connected ;
+                      this.setState({connected : newStatus}) ;
+        }}  className="status">
+          <p className={this.state.connected ? "status-online" : "status-offline"}> </p>
+          <p className="status-text"> {this.state.connected ? "OnLine" : "OffLine"} </p>
         </div>
       </div>
     </div>
-  );
+  );}
+  
 };
 
 export default Contact;
